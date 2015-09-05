e24137190a317b0266f755f8f93f316aefdc58b5
Felix Bruckmeier <felix.m.bruckmeier@gmail.com>
2015-09-05 12:06:32 +0200

    change: logging and hostname

--- src/main/scala/SimpleClusterListener.scala
+++ src/main/scala/SimpleClusterListener.scala
@@ -15,13 +15,13 @@ class SimpleClusterListener extends Actor with ActorLogging {
 
   def receive = {
     case MemberUp(member) =>
-      log.info("Member is now up: {}", member.address)
+      println(s"Member is now up: ${member.address}")
 
     case UnreachableMember(member) =>
-      log.info("Member detected as unreachable: {}", member)
+      println(s"Member detected as unreachable: $member")
 
     case MemberRemoved(member, previousStatus) =>
-      log.info("Member is removed: {} after {}", member.address, previousStatus)
+      println(s"Member is removed: ${member.address} after $previousStatus")
 
     case _: MemberEvent => // ignore
   }
